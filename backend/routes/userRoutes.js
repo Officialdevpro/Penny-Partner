7a3b631313165ac4ff2bb3d2a9dfbc3784d96181
Officialdevpro <official.devpro@gmail.com>
2025-01-13 12:01:47 +0530

    update password icons

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -36,7 +36,7 @@ const rateLimit = require("express-rate-limit");
 
 
 const emailLimiter = rateLimit({
-  max: 2,
+  max: 10,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again in an hour!",
   handler: (req, res, next, options) => {
